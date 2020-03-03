fd924944ea874bb7ed836e225b33cb53225abd4e
ailequal <37016865+ailequal@users.noreply.github.com>
2020-03-03 16:25:09 +0100

    Added part two

--- query.sql
+++ query.sql
@@ -1,3 +1,4 @@
+-- Queres - Parte 01
 -- Seleziona tutti gli ospiti che sono stati identificati con la carta di identità
 SELECT `*`
 FROM `ospiti`
@@ -44,3 +45,46 @@ OR `ospite_id` <> '';
 -- Quanti posti letto ha l’hotel in totale?
 SELECT SUM(beds)
 FROM `stanze`;
+
+
+-- Group by - Parte 02
+-- Conta gli ospiti raggruppandoli per anno di nascita
+SELECT EXTRACT(YEAR FROM `date_of_birth`) `year`, COUNT(`id`) `total`
+FROM `ospiti`
+GROUP BY EXTRACT(YEAR FROM `date_of_birth`);
+
+-- Somma i prezzi dei pagamenti raggruppandoli per status
+SELECT `status`, SUM(price) `total`
+FROM `pagamenti`
+GROUP BY `status`;
+
+-- Conta quante volte è stata prenotata ogni stanza
+SELECT `stanza_id` `room`, COUNT(`id`) `total_reservation`
+FROM `prenotazioni`
+GROUP BY `stanza_id`;
+
+-- Fai una analisi per vedere se ci sono ore in cui le prenotazioni sono più frequenti
+SELECT EXTRACT(HOUR FROM `created_at`) `hours`, COUNT(`id`) `total`
+FROM `prenotazioni`
+GROUP BY EXTRACT(HOUR FROM `created_at`)
+ORDER BY `total` DESC;
+
+-- Quante prenotazioni ha fatto l’ospite che ha fatto più prenotazioni?
+SELECT `ospite_id`, COUNT(`prenotazione_id`) `total`
+FROM `prenotazioni_has_ospiti`
+GROUP BY `ospite_id`;
+
+
+-- Join - Parte 03
+-- Come si chiamano gli ospiti che hanno fatto più di due prenotazioni?
+
+-- Stampare tutti gli ospiti per ogni prenotazione
+
+-- Stampare Nome, Cognome, Prezzo e Pagante per tutte le prenotazioni fatte a Maggio 2018
+
+-- Fai la somma di tutti i prezzi delle prenotazioni per le stanze del primo piano
+
+-- Prendi i dati di fatturazione per la prenotazione con id=7
+
+-- Le stanze sono state tutte prenotate almeno una volta? (Visualizzare le stanze non ancora prenotate)
+
